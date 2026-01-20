4cae880fc0c17d04c3a305bc772bc2bbdadbcf87
ArgenisBouzas <argenisjb@gmail.com>
2026-01-20 10:45:52 -0400

    10.3

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,10 +6,11 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 export default function Search({ placeholder }: { placeholder: string }) {
 
     const searchParams = useSearchParams();
-    function handleSearch(term: string) {
       const params = new URLSearchParams(searchParams);
       const pathname = usePathname();
       const { replace } = useRouter();
+    function handleSearch(term: string) {
+    
       if (term) {
         params.set('query', term);
       } else {
@@ -29,6 +30,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
